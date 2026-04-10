b69a9968e13e4931e847e63bc7afc2b9472d769f
ayushwing <sayushsoni@gmail.com>
2026-04-10 23:55:31 +0530

    feat: add user subgraph with schema and resolvers

---
+++ user-subgraph/src/main/java/com/ayushwing/federation/user/UserSubgraphApplication.java
@@ -0,0 +1,12 @@
+package com.ayushwing.federation.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubgraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSubgraphApplication.class, args);
+    }
+}
